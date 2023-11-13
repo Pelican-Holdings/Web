5c9736cc334fd5f8c3543d33d38bcd2a8b7878f5
YohanNayanajith <yohannayanajith40@gmail.com>
2023-11-13 20:59:24 +0530

    bug/change header image

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
       <header className="w-full z-40 bg-main">
         <div className="max-w-[1440px] mx-auto sm:px-16 px-6 py-3 bg-transparent text-white">
           <Image
-            src="/barrel.png"
+            src="https://res.cloudinary.com/midefulness/image/upload/v1699886718/Pelican%20Holdings/barrel_jpshgf.png"
             alt="menu"
             width={24}
             height={24}
